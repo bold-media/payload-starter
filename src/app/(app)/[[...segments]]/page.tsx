23a4f0c744e885f4c929fba828b609361fc7ee96
Harley Salas <harley@harleysalas.com>
2024-12-11 21:34:35 +0300

    feat: generate metadata

--- src/app/(app)/[[...segments]]/page.tsx
+++ src/app/(app)/[[...segments]]/page.tsx
@@ -1,11 +1,13 @@
 import { resolvePathname } from '@/utils/resolvePathname'
 import { draftMode } from 'next/headers'
-import { getPayload, Where } from 'payload'
+import { getPayload } from 'payload'
 import React, { cache } from 'react'
 import config from '@payload-config'
 import { notFound } from 'next/navigation'
 import { RichText } from '@/modules/common/RichText'
 import { Metadata, ResolvingMetadata } from 'next'
+import { generateMeta } from '@/utils/generateMeta'
+// import { getPathSegments } from '@/utils/getPathSegments'
 
 interface Props {
   params: Promise<{
@@ -33,43 +35,66 @@ export default Pages
 
 const queryPageByPathname = cache(async (pathname: string) => {
   try {
+    console.log(`queryPageByPathname: ${pathname}`)
     const { isEnabled: draft } = await draftMode()
     const payload = await getPayload({ config })
 
-    const query: Where = draft
-      ? { pathname: { equals: pathname } }
-      : {
-          and: [{ _status: { equals: 'published' } }, { pathname: { equals: pathname } }],
-        }
-
-    const data = await payload.find({
+    const { docs } = await payload.find({
       collection: 'page',
       draft,
-      where: query,
+      limit: 1,
+      pagination: false,
+      overrideAccess: draft,
+      disableErrors: true,
+      where: {
+        pathname: {
+          equals: pathname,
+        },
+      },
     })
-    return data.docs?.[0] ?? null
+    return docs?.[0] || null
   } catch (error) {
     return null
   }
 })
 
+/**
+ * for when using PPR
+ */
+// export const generateStaticParams = async () => {
+//   try {
+//     const payload = await getPayload({config})
+//     const pages = await payload.find({
+//       collection: "page",
+//       draft: false,
+//       limit: 0,
+//       overrideAccess: false,
+//       select: {
+//         pathname: true
+//       }
+//     })
 
-export const generateMetadata = async ({params}: Props, parentPromise: ResolvingMetadata): Promise<Metadata> => {
-  const { segments } = await params;
+//     const paths = pages?.docs
+//     ?.filter((page): page is typeof page & { pathname: string } =>
+//       typeof page?.pathname === 'string'
+//     )
+//     .map((page) => ({
+//       segments: getPathSegments(page.pathname)
+//     }))
+//   } catch (error) {
+//     return []
+//   }
+// }
+
+export const generateMetadata = async (
+  { params }: Props,
+  parentPromise: ResolvingMetadata,
+): Promise<Metadata> => {
+  const { segments } = await params
   const pathname = resolvePathname(segments)
-  const page = await queryPageByPathname(pathname);
-  const parent = await parentPromise;
+  const page = await queryPageByPathname(pathname)
 
-  const ogImage = typeof page?.meta?.image === 'object' && page?.meta?.image !== null && 'url' in page?.meta?.image && `${page.meta.image.url}`;
+  const fallback = await parentPromise
 
-  return {
-    title: page?.meta?.title || parent.title,
-    description: page?.meta?.description || parent?.description,
-    openGraph: {
-      title: page?.meta?.title || parent.title || '',
-      description: page?.meta?.description || parent?.description || '',
-      url: `${process.env.NEXT_PUBLIC_APP_URL}${page?.pathname}`,
-      images: typeof ogImage === 'string' ? ogImage : undefined
-    }
-  }
+  return generateMeta({ meta: page?.meta, fallback, pathname })
 }
