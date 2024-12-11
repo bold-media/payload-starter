77cdcc556ee3fc3850c748313a45bfc485288e2e
Harley Salas <harley@harleysalas.com>
2024-12-11 14:26:41 +0300

    cleanup

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -1,6 +1,5 @@
 // storage-adapter-import-placeholder
 import { postgresAdapter } from '@payloadcms/db-postgres'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
