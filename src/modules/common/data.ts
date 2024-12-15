d7d63f27a086a683823c15a1ed40c1f8318302e6
Harley Salas <harley@harleysalas.com>
2024-12-15 20:08:25 +0300

    use cache util

--- src/modules/common/data.ts
+++ src/modules/common/data.ts
@@ -1,10 +1,44 @@
-import { unstable_cache } from "next/cache";
-import { getPayload } from "payload";
-import { cache } from "react";
-import config from '@payload-config';
+import { getPayload } from 'payload'
+import config from '@payload-config'
+import { draftMode } from 'next/headers'
+import { cache } from '@/utils/cache'
 
-export const getSettings = cache(unstable_cache(async () => {
+export const getSettings = cache(
+  async () => {
     const payload = await getPayload({ config })
-    const settings = await payload.findGlobal({slug: "settings"}).then((res) => res).catch(() => null);
+    const settings = await payload
+      .findGlobal({ slug: 'settings' })
+      .then((res) => res)
+      .catch(() => null)
     return settings
-}, ['getSettings'], {tags: ["settings"]}))
+  },
+  { tags: [`settings`] },
+)
+
+export const getPageByPathname = cache(
+  async (pathname: string) => {
+    try {
+      console.log(`queryPageByPathname: ${pathname}`)
+      const { isEnabled: draft } = await draftMode()
+      const payload = await getPayload({ config })
+
+      const { docs } = await payload.find({
+        collection: 'page',
+        draft,
+        limit: 1,
+        pagination: false,
+        overrideAccess: draft,
+        disableErrors: true,
+        where: {
+          pathname: {
+            equals: pathname,
+          },
+        },
+      })
+      return docs?.[0] || null
+    } catch (error) {
+      return null
+    }
+  },
+  { tags: (pathname) => [pathname] },
+)
