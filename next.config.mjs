241103378c3a4e511ef42903ce588291699cda5d
Harley Salas <harley@harleysalas.com>
2024-12-17 15:17:56 +0300

    comment out ppr

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,9 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
-  experimental: {
-    ppr: 'incremental',
-    dynamicIO: true,
-  },
+  // experimental: {
+  //   ppr: 'incremental',
+  // },
 }
 
 export default withPayload(nextConfig)
