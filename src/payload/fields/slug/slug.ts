346ed00fbb22b4594bd20eee00bc83d4da26f2c2
Harley Salas <harley@harleysalas.com>
2025-01-08 01:44:04 +0300

    support document duplication with slug

--- src/payload/fields/slug/slug.ts
+++ src/payload/fields/slug/slug.ts
@@ -3,24 +3,34 @@ import format from 'standard-slugify'
 import { deepMerge } from '@/utils/deepMerge'
 import { en } from '@/payload/i18n/en'
 import { ru } from '@/payload/i18n/ru'
+import { generateShortId } from '@/utils/generateShortId'
 
 type Slug = (fieldToUse?: string, overrides?: Partial<Field>) => Field
 
 const formatSlug =
   (field: string): FieldHook =>
-  ({ operation, value, previousValue, data }) => {
-    if ((operation === 'create' || operation === 'update') && value !== previousValue) {
+  ({ operation, value, previousValue, data, context }) => {
+    if (
+      (operation === 'create' || operation === 'update') &&
+      value !== previousValue &&
+      !context.duplicate
+    ) {
       if (value === '/') {
         return value
       }
 
       //slug was manually entered, format it.
-      if (typeof value === 'string') {
+      if (typeof value === 'string' && value?.length > 0) {
         return format(value)
       }
     }
 
-    if ((operation === 'create' || operation === 'update') && !value && data?.[field]) {
+    if (
+      (operation === 'create' || operation === 'update') &&
+      !value &&
+      data?.[field] &&
+      !context.duplicate
+    ) {
       return format(data?.[field])
     }
   }
@@ -41,6 +51,15 @@ export const slug: Slug = (fieldToUse = 'title', overrides = {}) =>
       },
       hooks: {
         beforeValidate: [formatSlug(fieldToUse)],
+        beforeDuplicate: [
+          ({ value, context }) => {
+            context.duplicate = true
+            if (value === '/') {
+              return generateShortId()
+            }
+            return `${value}-${generateShortId()}`
+          },
+        ],
       },
     },
     overrides,
