613a95d26499773197144e309b17b56b2262fd33
Harley Salas <harley@harleysalas.com>
2025-01-10 15:39:16 +0300

    git rid of old migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,1 @@
-import * as migration_20241217_123954 from './20241217_123954';
-
-export const migrations = [
-  {
-    up: migration_20241217_123954.up,
-    down: migration_20241217_123954.down,
-    name: '20241217_123954'
-  },
-];
+export const migrations = []
